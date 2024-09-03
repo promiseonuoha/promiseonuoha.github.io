fcbe3909c8a9d008736dc3d1db5afec592513921
unknown <pebuka351@gmail.com>
2024-09-03 16:26:43 +0100

    Build

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,13 +7,13 @@ document
     const formData = {
       name: document.getElementById("name-2").value,
       email: document.getElementById("Email-3").value,
-      project: document.getElementById("Project-4").value,
+      serviceNeeded: document.getElementById("Project-4").value,
       budget: document.getElementById("Budget-2").value,
-      date: document.getElementById("Date-2").value,
+      timeline: document.getElementById("Date-2").value,
     };
 
     // Send form data to the server
-    fetch("your-server-endpoint", {
+    fetch("https://send-email-q58w.onrender.com/api/v1/email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,14 +22,13 @@ document
     })
       .then((response) => {
         if (response.ok) {
-          document.querySelector(".success-message").style.display = "block";
-          document.querySelector(".error-message").style.display = "none";
+          document.querySelector(".contact-form-done").style.display = "block";
+          document.querySelector(".contact-form-fail").style.display = "none";
         } else {
           throw new Error("Something went wrong");
         }
       })
       .catch((error) => {
-        document.querySelector(".success-message").style.display = "none";
-        document.querySelector(".error-message").style.display = "block";
+        document.querySelector(".contact-form-fail").style.display = "block";
       });
   });
